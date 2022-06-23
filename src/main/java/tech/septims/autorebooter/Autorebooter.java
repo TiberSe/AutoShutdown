0831b5a7eba67d8a3fa432fed9e3a4cf03c454c0
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-06-23 14:32:58 +0900

    Initial commit

---
+++ src/main/java/tech/septims/autorebooter/Autorebooter.java
@@ -0,0 +1,17 @@
+package tech.septims.autorebooter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Autorebooter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
