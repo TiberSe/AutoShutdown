c5fe12ef0aef6d00d50c73a054b366706c0e1ce6
TiberSe <82593491+TiberSe@users.noreply.github.com>
2022-07-02 19:01:20 +0900

    set default value

--- src/main/java/tech/septims/autorebooter/utils/RebooterConfig.kt
+++ src/main/java/tech/septims/autorebooter/utils/RebooterConfig.kt
@@ -12,13 +12,13 @@ class RebooterConfig : ConfigBase {
     }
 
     fun getTickThreshold(): Double {
-        return rebooterConfig.getDouble("tick-threshold")
+        return rebooterConfig.getDouble("tick-threshold", 9.5)
     }
     fun getShutdownDelayTime(): Long {
-        return rebooterConfig.getLong("shutdown-delay-time")
+        return rebooterConfig.getLong("shutdown-delay-time", 60)
     }
     fun getCheckInterval(): Long {
-        return rebooterConfig.getLong("check-interval")
+        return rebooterConfig.getLong("check-interval", 180)
     }
     fun getTickReference(): Int {
         if(rebooterConfig.getInt("tick-reference") in 0..3){
@@ -27,16 +27,16 @@ class RebooterConfig : ConfigBase {
         return 1
     }
     fun getRecoveryRecover(): Boolean{
-        return rebooterConfig.getBoolean("tick-recovery-recover")
+        return rebooterConfig.getBoolean("tick-recovery-recover", true)
     }
     fun getRecoveryThreshold(): Double{
-        return rebooterConfig.getDouble("tick-recovery-threshold")
+        return rebooterConfig.getDouble("tick-recovery-threshold", 15.5)
     }
     fun getRecoveryRecoverDelay(): Boolean{
-        return rebooterConfig.getBoolean("tick-recovery-recover-delay")
+        return rebooterConfig.getBoolean("tick-recovery-recover-delay", false)
     }
     fun getRecoveryRecoverDelayTime(): Long{
-        return rebooterConfig.getLong("recovery-recover-delay-time")
+        return rebooterConfig.getLong("recovery-recover-delay-time", 60)
     }
 
 }
